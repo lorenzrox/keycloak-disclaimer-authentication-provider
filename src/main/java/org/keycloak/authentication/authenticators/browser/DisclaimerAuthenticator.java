8bed8af8d57bf9534466524c4d358a246e74ecf2
Lorenzo Rossoni <l.rossoni@selecta.ve.it>
2023-11-23 17:22:42 +0100

    initial commit

---
+++ src/main/java/org/keycloak/authentication/authenticators/browser/DisclaimerAuthenticator.java
@@ -0,0 +1,50 @@
+package org.keycloak.authentication.authenticators.browser;
+
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.Response;
+
+public class DisclaimerAuthenticator implements Authenticator {
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public void authenticate(AuthenticationFlowContext context) {
+        Response challengeResponse = context.form()
+                .setExecution(context.getExecution().getId())
+                .createForm("disclaimer.ftl");
+        context.challenge(challengeResponse);
+    }
+
+    @Override
+    public void action(AuthenticationFlowContext context) {
+        MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
+        if (!formData.containsKey("accept")) {
+            context.cancelLogin();
+            return;
+        }
+
+        context.success();
+    }
+
+    @Override
+    public boolean requiresUser() {
+        return true;
+    }
+
+    @Override
+    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+        return true;
+    }
+
+    @Override
+    public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+    }
+
+}
---
+++ src/main/java/org/keycloak/authentication/authenticators/browser/DisclaimerAuthenticatorFactory.java
@@ -0,0 +1,76 @@
+package org.keycloak.authentication.authenticators.browser;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+
+public class DisclaimerAuthenticatorFactory implements AuthenticatorFactory {
+    public static final String PROVIDER_ID = "login-disclaimer";
+    private static DisclaimerAuthenticator SINGLETON = new DisclaimerAuthenticator();
+
+    @Override
+    public String getHelpText() {
+        return "Asks the user to accept disclaimer before submitting its login form.";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return null;
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Accept disclaimer";
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return "disclaimer";
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return REQUIREMENT_CHOICES;
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
+
+    @Override
+    public Authenticator create(KeycloakSession session) {
+        return SINGLETON;
+    }
+}
